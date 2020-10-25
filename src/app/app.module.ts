cf8381458e6ecb00fd6b6d25f458833043437dc6
Joe Cui <joe.cui@bpanz.com>
2020-10-25 10:52:39 +1000

    Connecting modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherModule } from './weather/weather.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, WeatherModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ForecastComponent } from './forecast/forecast.component';
 
-
-
 @NgModule({
   declarations: [ForecastComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [ForecastComponent],
 })
-export class WeatherModule { }
+export class WeatherModule {}
