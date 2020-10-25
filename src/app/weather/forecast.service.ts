325a5d758caea85ee6c8f0fdf618d9844e62c1dd
Joe Cui <joe.cui@bpanz.com>
2020-10-25 13:46:03 +1000

    Loading Forcast Service with Dependency Injection

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ForecastService {
+  constructor() {}
 
-  constructor() { }
+  getCurrentLocation() {
+    return new Observable<Coordinates>((observer) => {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          observer.next(position.coords);
+          observer.complete(); // no more data will be emitted
+        },
+        // .error() put entire observable into "Error" state, we can no longer emit event from this observable
+        (err) => observer.error(err)
+      );
+    });
+  }
 }
--- src/app/weather/forecast/forecast.component.ts
+++ src/app/weather/forecast/forecast.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ForecastService } from '../forecast.service';
 
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
-  styleUrls: ['./forecast.component.css']
+  styleUrls: ['./forecast.component.css'],
 })
 export class ForecastComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private forecastService: ForecastService) {
+    forecastService.getCurrentLocation().subscribe((coords) => {
+      console.log(coords);
+    });
   }
 
+  ngOnInit(): void {}
 }
