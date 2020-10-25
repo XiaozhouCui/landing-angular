99e5d4bad3fcd4251bca832bea4df02845e3bcf1
Joe Cui <joe.cui@bpanz.com>
2020-10-25 17:09:37 +1000

    Added pluck, mergeMap and filter operators

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, mergeMap, switchMap, pluck, filter } from 'rxjs/operators';
 import { HttpClient, HttpParams } from '@angular/common/http';
+
+interface OpenWeatherResponse {
+  list: {
+    dt_txt: string;
+    main: {
+      tem: number;
+    };
+  }[];
+}
 
 @Injectable({
   providedIn: 'root',
@@ -20,8 +29,16 @@ export class ForecastService {
           .set('units', 'metric')
           .set('appid', '6ed7aab3292b4cad763b859b79714283');
       }),
-      // switchMap will hijack the value emitted from original observable (getCurrentLocation), then return a new observable (http.get) and forget all previous values in the pipe
-      switchMap((params) => this.http.get(this.url, { params: params }))
+      // switchMap will hijack the value emitted from original observable (getCurrentLocation), then return a new observable (http.get) and discard all previous values in the pipe
+      switchMap((params) =>
+        this.http.get<OpenWeatherResponse>(this.url, { params: params })
+      ),
+      // all value after switchMap will be the http response emitted from new observable, forget about getCurrentLocation
+      pluck('list'), // only emit the response.list (an array of 40 objects)
+      // mergeMap is similar to switchMap, but will keep all returned observables
+      mergeMap((value) => of(...value)), // "of" is an observable, it will take in every array element as args and emit them one by one
+      // use "filter" to pick 1 element from every 8 resultes in the array
+      filter((value, index) => index % 8 === 0)
     );
   }
 
