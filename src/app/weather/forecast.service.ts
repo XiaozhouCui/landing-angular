4aa5e5fa1de7a2eb31fa8c53dfbdc7056f9a008c
Joe Cui <xiaozhou.cui@gmail.com>
2020-10-27 16:50:22 +1000

    Added catchError and throwError

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import {
   map,
   mergeMap,
@@ -8,8 +8,12 @@ import {
   filter,
   toArray,
   share,
+  tap,
+  catchError,
+  retry,
 } from 'rxjs/operators';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { NotificationsService } from '../notifications/notifications.service';
 
 interface OpenWeatherResponse {
   list: {
@@ -25,7 +29,10 @@ interface OpenWeatherResponse {
 })
 export class ForecastService {
   private url = 'https://api.openweathermap.org/data/2.5/forecast';
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private notificationsService: NotificationsService
+  ) {}
 
   getForecast() {
     return this.getCurrentLocation().pipe(
@@ -44,7 +51,7 @@ export class ForecastService {
       // all value after switchMap will be the http response emitted from new observable, forget about getCurrentLocation
       pluck('list'), // only emit the response.list (an array of 40 objects)
       // mergeMap is similar to switchMap, but will return and keep all 40 observables
-      mergeMap((value) => of(...value)), // "of" is an observable, it will take in every array element as args and emit them one by one
+      mergeMap((value) => of(...value)), // "of" returns a reconfigured observable, it will take in every array element as args and emit them one by one
       // use "filter" to pick 1 element in every 8 resultes to emit 5 weather objects
       filter((value, index) => index % 8 === 0),
       // convert the 5 huge objects into 5 smaller objects.
@@ -63,14 +70,35 @@ export class ForecastService {
 
   getCurrentLocation() {
     return new Observable<Coordinates>((observer) => {
+      // console.log('Trying to get location...');
       window.navigator.geolocation.getCurrentPosition(
         (position) => {
-          observer.next(position.coords);
+          observer.next(position.coords); // emit a value
           observer.complete(); // no more data will be emitted
         },
-        // .error() put entire observable into "Error" state, we can no longer emit event from this observable
-        (err) => observer.error(err)
+        // .error() put entire observable into "Error" state, we can no longer emit values from this observable
+        (err) => observer.error(err) // emit an error
       );
-    });
+    }).pipe(
+      retry(1),
+      tap(
+        // first arg of tap is called when observerable emits a value
+        () => {
+          this.notificationsService.addSuccess('Got your location');
+        }
+        // // second arg of tap is called when observerable is in error state
+        // (err) => {
+        //   this.notificationsService.addError('Failed to get your location');
+        // }
+      ),
+      // catchError operator will only be invoked when source observerable emits an error
+      catchError((err) => {
+        // 1) Handle the error
+        this.notificationsService.addError('Failed to get your location');
+
+        // 2) Return a new observable
+        return throwError(err); // EQUAVALENT: return new Observable(observer => {observer.error(err)});
+      })
+    );
   }
 }
