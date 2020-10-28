5697cf4c9659ce2d97ac33e04e45776ffde868af
Joe Cui <xiaozhou.cui@gmail.com>
2020-10-28 14:40:34 +1000

    Declared Subjects and built HTTP Params in news-api service

--- src/app/news-api/news-api.service.ts
+++ src/app/news-api/news-api.service.ts
@@ -1,4 +1,7 @@
+import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { tap, map, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +12,25 @@ export class NewsApiService {
   private apiKey = '454e66fd1bb44fa4b7901db0d749a967';
   private country = 'au';
 
-  constructor() {}
+  // pagesInput is the selected page number in pagination
+  pagesInput: Subject<number>; // Subject is a generic type, requires 1 type arg (number)
+  // pagesOutput observable will emit article list
+  pagesOutput: Observable<any>; // Observable is a generic type, requires 1 type arg (type arg from response)
+  // total number of pages in pagination
+  numberOfPages: Observable<number>;
+
+  constructor() {
+    this.pagesInput = new Subject();
+    // by chaining .pipe(), "pagesInput" Subject will become Observable (cold and unicast)
+    this.pagesOutput = this.pagesInput.pipe(
+      // "page" is what is thrown into the "pagesInput" Subject
+      map((page) => {
+        return new HttpParams()
+          .set('apiKey', this.apiKey)
+          .set('country', this.country)
+          .set('pageSize', String(this.pageSize))
+          .set('page', String(page));
+      })
+    );
+  }
 }
